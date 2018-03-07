f5476926e14242317a4b9caf7dd63756228e89ac
Weicheng-mac <weich@iastate.edu>
2018-03-07 15:47:00 -0600

    msk.tol=0.95

--- R/gapfill.R
+++ R/gapfill.R
@@ -20,11 +20,12 @@
 #' @examples 
 gapfill <- function(year, doy, mat, img.nrow, img.ncol, h,
                     doyrange = seq(min(doy), max(doy)), nnr, method = c("lc", "emp"),
-                    pve = 0.99, outlier.tol = 0.2, outlier.action = c("ac", "keep"), mc.cores = parallel::detectCores()){
+                    pve = 0.99, outlier.tol = 0.2, outlier.action = c("ac", "keep"),
+                    msk.tol = 0.95, mc.cores = parallel::detectCores()){
 
   idx = 1:length(year) ## idx is the index of image, 1, 2, 3,...
   registerDoParallel(cores = mc.cores)
-  msk = getMask(mat) # idx for 'black holes'
+  msk = getMask(mat, tol = msk.tol) # idx for 'black holes'
   N = img.nrow * img.ncol ## total number of pixels (including black holes)
   N1 = sum(!msk) # number of 'actual' pixels for each image (except for black holes)
   if(N1 == 0){ ## a black hole image
