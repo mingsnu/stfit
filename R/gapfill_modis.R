3a429715aad361e9f2209a48500c9decd182c478
Weicheng-mac <weich@iastate.edu>
2018-04-24 10:30:06 -0500

    gapfill_modis no cluster option

--- R/gapfill_modis.R
+++ R/gapfill_modis.R
@@ -1,3 +1,32 @@
+#' Title
+#'
+#' @param doy 
+#' @param mat 
+#' @param img.nrow 
+#' @param img.ncol 
+#' @param doyeval 
+#' @param h.tcov 
+#' @param h.tsigma2 
+#' @param h.scov 
+#' @param h.ssigma2 
+#' @param nnr 
+#' @param msk vector of mask
+#' @param ncluster number of clusters. If 0 no cluster analysis is performed and the mean is estimated based on
+#' pixel level.
+#' @param max.per.cluster maximum number of pixels to select for covariance estimation.
+#' @param t.grid.num number of grid on doyeval, on which to estimate the covariance matrix.
+#' @param breaks if NULL, estimate spatial effect on the whole image; if a list with 'block.nrow', 'block.ncol',
+#' 'img.nrow' and 'img.ncol', the image is broken down into small blocks and do estimation seperately.
+#' @param outlier.action 
+#' @param intermediate.save TRUE or FALSE. Whether to save intermediate results.
+#' @param intermediate.dir Path to save intermediate results.
+#' @param teff TRUE or FALSE. Whether to estimate temporal effect.
+#' @param seff TRUE or FALSE. Whether to estimate spatial effect.
+#'
+#' @return
+#' @export
+#'
+#' @examples
 gapfill_modis <- function(doy, mat, img.nrow, img.ncol, doyeval = 1:365, h.tcov = 100, 
                                   h.tsigma2 = 300, h.scov = 2, h.ssigma2 = 2, nnr = 10, 
                                   msk = NULL, ncluster = 50, max.per.cluster = 30, t.grid.num = 50,
@@ -22,29 +51,31 @@ gapfill_modis <- function(doy, mat, img.nrow, img.ncol, doyeval = 1:365, h.tcov 
   meanest = meanEst(doy, mat, doyeval = 1:365, msk = msk)
   if(intermediate.save)
     saveRDS(meanest, paste0(intermediate.dir, "meanest.rds"))
-  #####################################################
-  #### 2. Cluster analysis based on inital results ####
-  #####################################################
-  cmat = t(meanest$meanmat[seq(10, 350, by = 10),!msk])
-  cres = kmeans(cmat, centers = ncluster, nstart = 10, iter.max = 50)
-  cluster = rep(0, ncol(mat))
-  cluster[!msk] = cres$cluster
-  if(intermediate.save)
-    saveRDS(cluster, paste0(intermediate.dir, "cluster.rds"))
-  
-  ################################################
-  #### 3. Overall mean estimaton with cluster ####
-  ################################################
-  ## NEVER USE smooth_spline when using clusters
-  meanest_cl = meanEst(doy, mat, doyeval = doyeval, cluster = cluster, msk = msk)
-  if(intermediate.save)
-    saveRDS(meanest_cl, paste0(intermediate.dir, "meanest_cl.rds"))
+  if(ncluster > 0){
+    #####################################################
+    #### 2. Cluster analysis based on inital results ####
+    #####################################################
+    cmat = t(meanest$meanmat[seq(10, 350, by = 10),!msk])
+    cres = kmeans(cmat, centers = ncluster, nstart = 10, iter.max = 50)
+    cluster = rep(0, ncol(mat))
+    cluster[!msk] = cres$cluster
+    if(intermediate.save)
+      saveRDS(cluster, paste0(intermediate.dir, "cluster.rds"))
+    ################################################
+    #### 3. Overall mean estimaton with cluster ####
+    ################################################
+    ## NEVER USE smooth_spline when using clusters
+    meanest_cl = meanEst(doy, mat, doyeval = doyeval, cluster = cluster, msk = msk)
+    if(intermediate.save)
+      saveRDS(meanest_cl, paste0(intermediate.dir, "meanest_cl.rds"))
+  } else
+    meanest_cl = meanest
 
   #########################################
   #### 2. 'temporal' effect estimation ####
   #########################################
   ## remove outlier images
-  outlier.img.idx = meanest$idx$idx.outlier
+  outlier.img.idx = meanest_cl$idx$idx.outlier
 
   for(i in outlier.img.idx){
     mat[outlier.img.idx,] = NA
@@ -78,38 +109,35 @@ gapfill_modis <- function(doy, mat, img.nrow, img.ncol, doyeval = 1:365, h.tcov 
   ######################################
   ## estimate the spatial effect using residuals
   if(seff){
-    nblocks = breaks$block.nrow*breaks$block.ncol
-    res3.list = foreach(n=1:nblocks) %dopar% {
-      ii = floor((n-1)/breaks$block.ncol) + 1
-      jj = (n-1) %% breaks$block.ncol + 1
-      ## block index
-      bIdx = c(t(outer(seq((ii-1)*breaks$img.nrow+1, ii*breaks$img.nrow), 
-                       seq((jj-1)*breaks$img.ncol+1, jj*breaks$img.ncol),
-                       FUN = function(ridx, cidx){
-                         (ridx-1) * img.ncol + cidx
-                       })))
-      seffEst(rmat[, bIdx], breaks$img.nrow, breaks$img.ncol, nnr = nnr, 
-              h.cov = h.scov, h.sigma2 = h.ssigma2, msk = msk[bIdx])
-    }
-    ## saveRDS(res3.list, "./output/res3_list.rds")
-    #### visualize residials after doing spatial imputation
-    # aa = seffEst(rmat[, bIdx], 30, 30, nnr = 3, msk = msk[bIdx])
-    # seff_stack1 = mat2stack(aa$seffmat, 30)
-    # levelplot(seff_stack1[[seq(1, 365, by = 20)]], par.settings = colthm)
-    # tmp = mat2stack(rmat[, bIdx], 30)
-    # levelplot(tmp[[seq(1, 365, by = 20)]], par.settings = colthm)
-    
-    seffmat = matrix(NA, nrow(rmat), ncol(rmat))
-    for(n in 1:nblocks){
-      ii = floor((n-1)/breaks$block.ncol) + 1
-      jj = (n-1) %% breaks$block.ncol + 1
-      ## block index
-      bIdx = c(t(outer(seq((ii-1)*breaks$img.nrow+1, ii*breaks$img.nrow), 
-                       seq((jj-1)*breaks$img.ncol+1, jj*breaks$img.ncol),
-                       FUN = function(ridx, cidx){
-                         (ridx-1) * img.ncol + cidx
-                       })))
-      seffmat[,bIdx] = res3.list[[n]]$seffmat
+    if(is.null(breaks)){
+      seffmat = seffEst(rmat, img.nrow, img.ncol, nnr = nnr, h.cov = h.scov, h.sigma2 = h.ssigma2, msk = msk)$sseffmat
+    } else{
+      nblocks = breaks$block.nrow*breaks$block.ncol
+      res3.list = foreach(n=1:nblocks) %dopar% {
+        ii = floor((n-1)/breaks$block.ncol) + 1
+        jj = (n-1) %% breaks$block.ncol + 1
+        ## block index
+        bIdx = c(t(outer(seq((ii-1)*breaks$img.nrow+1, ii*breaks$img.nrow), 
+                         seq((jj-1)*breaks$img.ncol+1, jj*breaks$img.ncol),
+                         FUN = function(ridx, cidx){
+                           (ridx-1) * img.ncol + cidx
+                         })))
+        seffEst(rmat[, bIdx], breaks$img.nrow, breaks$img.ncol, nnr = nnr, 
+                h.cov = h.scov, h.sigma2 = h.ssigma2, msk = msk[bIdx])
+      }
+      
+      seffmat = matrix(NA, nrow(rmat), ncol(rmat))
+      for(n in 1:nblocks){
+        ii = floor((n-1)/breaks$block.ncol) + 1
+        jj = (n-1) %% breaks$block.ncol + 1
+        ## block index
+        bIdx = c(t(outer(seq((ii-1)*breaks$img.nrow+1, ii*breaks$img.nrow), 
+                         seq((jj-1)*breaks$img.ncol+1, jj*breaks$img.ncol),
+                         FUN = function(ridx, cidx){
+                           (ridx-1) * img.ncol + cidx
+                         })))
+        seffmat[,bIdx] = res3.list[[n]]$seffmat
+      }
     }
     if(intermediate.save)
       saveRDS(seffmat, paste0(intermediate.dir, "seffmat.rds"))
