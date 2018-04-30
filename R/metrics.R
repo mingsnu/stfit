80bd33de0b65ed908425411569420d32a5f0af8a
Weicheng-mac <weich@iastate.edu>
2018-04-29 23:56:36 -0500

    document of metrics

--- R/metrics.R
+++ R/metrics.R
@@ -1,9 +1,20 @@
+#' Error metrics
+#'
+#' @param y vector
+#' @param ypred vecotr
+#'
+#' @return numeric number. A measure of difference between y and ypred.
+#' @export
+#'
+#' @examples
 RMSE = function(y, ypred){
   sqrt(sum((y-ypred)^2)/length(y))
 }
+##' @rdname RMSE
 NMSE = function(y, ypred){
   sum((y-ypred)^2)/sum(y^2)
 }
+##' @rdname RMSE
 ARE = function(y, ypred){
   mean(abs(y - ypred)/y)
 }
