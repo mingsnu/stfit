ff05e7054e257b2dfe87dbdcee8b14390f4c6da9
Weicheng-mac <weich@iastate.edu>
2018-05-14 09:41:07 -0500

    add SAMP

---
+++ test/SAMP/README.md
@@ -0,0 +1,5 @@
+soil moisture data
+https://nsidc.org/data/search/#keywords=soil+moisture/sortKeys=score,,desc/facetFilters=%257B%257D/pageNumber=1/itemsPerPage=25
+
+- Before installing the h5 package, first need to install HDF5 library dependencies and headers:
+brew install hdf5 --enable-cxx
---
+++ test/SAMP/code/visualize.R
@@ -0,0 +1,9 @@
+library(raster)
+library(rasterVis)
+library(stfit)
+colthm = RdBuTheme()
+colthm$regions$col = rev(colthm$regions$col)
+
+smapdat = readRDS("../data/smapdat.rds")
+s = mat2stack(smapdat, 406, idx=1:365, xmn=-179.8133, xmx=179.8133, ymn=-83.63197, ymx=83.63197)
+levelplot(s[[1:6]], par.settings = colthm, layout=c(2,3))
