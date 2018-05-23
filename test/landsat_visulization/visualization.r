3d3cdc21f747fe48b10bad875743e7874d214c82
Weicheng-mac <weich@iastate.edu>
2018-05-23 03:19:53 -0500

    landsat 4 season new

--- test/landsat_visulization/visualization.r
+++ test/landsat_visulization/visualization.r
@@ -5,7 +5,7 @@ library(doParallel)
 library(Matrix)
 library(raster)
 library(rasterVis)
-library(Gapfill)
+library(stfit)
 df = read_feather("../data/features_106_wide.feather")
 df = df %>% filter(year >= 2000)
 year = df$year
@@ -101,13 +101,39 @@ dev.off()
 #   dev.off()
 # }
 
-## Based on the images, five fully observed images are chosen from each season: doy 1~96; 97~184; 185~273; 274~365
-fidx1 = c(261, 265, 312, 387, 581)
-fidx2 = c(145, 276, 444, 481, 587)
-fidx3 = c(198, 202, 493, 549, 557)
-fidx4 = c(82, 293, 505, 609, 615)
+# ## Based on the images, five fully observed images are chosen from each season: doy 1~96; 97~184; 185~273; 274~365
+# fidx1 = c(261, 265, 312, 387, 581)
+# fidx2 = c(145, 276, 444, 481, 587)
+# fidx3 = c(198, 202, 493, 549, 557)
+# fidx4 = c(82, 293, 505, 609, 615)
+# s = mat2stack(mat[c(fidx1, fidx2, fidx3, fidx4),], 31)
+# levelplot(s, par.settings = colthm, layout = c(5,4))
+
+## Four seasons ======
+##### Astronomical
+## Spring: March 20 ~ June 20:  DOY 80 ~ 171
+## Summer: June 21 ~ Sep 22: 172 ~ 265
+## Fall: Sep 23 ~ Dec 20: 266 ~ 354
+## Winter: Dec 21 ~ March 20: DOY 1 ~ 79; 354 ~ 365
+##### Meteorological season
+## Spring:March 1 ~ May 31: DOY 60 ~ 151
+## Summer: June 1 ~ August 31: DOY 152 ~ 243
+## Fall: Sep 1 ~ Nov 30: DOY 244 ~ 334
+## Winter: Dec 1 ~ Feb 28: DOY 335 ~ 365; 1 ~ 59
+full.idx = which(apply(mat, 1, function(x) all(!is.na(x))))
+spring.idx = full.idx[which(doy[full.idx] %in% 60:151)]
+summer.idx = full.idx[which(doy[full.idx] %in% 152:243)]
+fall.idx = full.idx[which(doy[full.idx] %in% 244:334)]
+winter.idx = full.idx[which(doy[full.idx] %in% c(1:59, 335:365))]
+spring.idx
+summer.idx
+fall.idx
+winter.idx
+
+fidx1 = c(145, 387, 481, 581, 587)
+fidx2 = c(198, 276, 444, 493, 549)
+fidx3 = c(82, 202, 293, 505, 557) #609 
+fidx4 = c(88, 132, 261, 265, 615) 
+
 s = mat2stack(mat[c(fidx1, fidx2, fidx3, fidx4),], 31)
 levelplot(s, par.settings = colthm, layout = c(5,4))
-
-
-
